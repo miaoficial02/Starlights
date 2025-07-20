7c1a2bc52b9184142c3ac30016a13dcf2103b7a3
𝑨𝑰𝑵𝒁 𝑶𝑶𝑨𝑳 𝑮𝑶𝑾𝑵 <brayanphonkponk@gmail.com>
2025-07-20 14:55:39 -0600

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -6,7 +6,7 @@ const youtubeRegexID = /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/))([a-z
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `*✨️ QUE MÚSICA QUIERES DESCARGAR*.`, m)
+      return conn.reply(m.chat, `*✨️ QUE MÚSICA QUIERES DESCARGAR*.`, m, fake)
     }
 
 let videoIdToFind = text.match(youtubeRegexID) || null
