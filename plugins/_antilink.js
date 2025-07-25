d27d84e413ccd17b43a968a5d5aae08d32730ed3
𝑨𝑰𝑵𝒁 𝑶𝑶𝑨𝑳 𝑮𝑶𝑾𝑵 <brayanphonkponk@gmail.com>
2025-07-24 22:06:30 -0600

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -26,7 +26,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, 
     }
 
     await conn.sendMessage(m.chat, {
-      text: `⚡✨ *「 𝑷𝒊𝒌𝒂𝒄𝒉𝒖 AntiLink 」* ✨⚡\n\n🔗 *Detecté un enlace sospechoso en el grupo...*\n\n👤 Usuario: ${user}\n❌ *Reglas violadas*\n\n💥 Serás eliminado en unos segundos...\n`,
+      text: `⚡✨ *「 Roxy-MD AntiLink 」* ✨⚡\n\n🔗 *Detecté un enlace sospechoso en el grupo...*\n\n👤 Usuario: ${user}\n❌ *Reglas violadas*\n\n💥 Serás eliminado en unos segundos...\n`,
       mentions: [m.sender]
     }, { quoted: m, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 });
 
