8d608f173d937bca44117b48f0294cef15afaa83
𝑨𝑰𝑵𝒁 𝑶𝑶𝑨𝑳 𝑮𝑶𝑾𝑵 <brayanphonkponk@gmail.com>
2025-07-24 22:05:58 -0600

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,32 +1,48 @@
-import fs from 'fs'
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex1 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 
-let warns = JSON.parse(fs.readFileSync('./data/warns.json'))
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
+  if (!m.isGroup) return;
+  if (isAdmin || isOwner || m.fromMe || isROwner) return;
 
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
-  if (!m.isGroup) return
-  if (!global.db.data.chats[m.chat].antilink) return
-  if (isAdmin) return // Ignora si es admin
+  let chat = global.db.data.chats[m.chat];
+  let delet = m.key.participant;
+  let bang = m.key.id;
+  const user = `@${m.sender.split`@`[0]}`;
+  const groupAdmins = participants.filter(p => p.admin);
+  const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n');
+  let bot = global.db.data.settings[this.user.jid] || {};
+  const isGroupLink = linkRegex.exec(m.text) || linkRegex1.exec(m.text);
+  const grupo = `https://chat.whatsapp.com`;
 
-  const regex = /(https?:\/\/)?(chat\.whatsapp\.com|t\.me\/joinchat|t\.me\/\w+)/i
-  if (regex.test(m.text)) {
-    await conn.sendMessage(m.chat, { delete: m.key }) // Elimina mensaje
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) {
+    return m.reply(`⚠️ *El sistema AntiLink está activo*, pero te salvaste por ser admin. 🎩`);
+  }
 
-    let id = m.sender
-    warns[m.chat] = warns[m.chat] || {}
-    warns[m.chat][id] = warns[m.chat][id] || 3
-    warns[m.chat][id]--
+  if (chat.antiLink && isGroupLink && !isAdmin) {
+    if (isBotAdmin) {
+      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+      if (m.text.includes(linkThisGroup)) return !0;
+    }
 
-    fs.writeFileSync('./data/warns.json', JSON.stringify(warns, null, 2))
+    await conn.sendMessage(m.chat, {
+      text: `⚡✨ *「 𝑷𝒊𝒌𝒂𝒄𝒉𝒖 AntiLink 」* ✨⚡\n\n🔗 *Detecté un enlace sospechoso en el grupo...*\n\n👤 Usuario: ${user}\n❌ *Reglas violadas*\n\n💥 Serás eliminado en unos segundos...\n`,
+      mentions: [m.sender]
+    }, { quoted: m, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 });
 
-    if (warns[m.chat][id] <= 0) {
-      await conn.groupParticipantsUpdate(m.chat, [id], 'remove') // Expulsa
-      delete warns[m.chat][id]
-      fs.writeFileSync('./data/warns.json', JSON.stringify(warns, null, 2))
-    } else {
-      await conn.sendMessage(m.chat, {
-        text: `🚫 *Antilink activado*\n@${id.split('@')[0]}, no se permiten links.\n⚠️ Advertencias restantes: *${warns[m.chat][id]}*`,
-        mentions: [id]
-      })
+    if (!isBotAdmin) {
+      return conn.sendMessage(m.chat, {
+        text: `⚠️ *El AntiLink está activo*, pero no puedo actuar porque *no soy administrador* del grupo.\n\n👑 *Admins del grupo:*\n${listAdmin}`,
+        mentions: [...groupAdmins.map(v => v.id)]
+      }, { quoted: m });
+    }
+
+    if (isBotAdmin) {
+      await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+      let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (responseb[0]?.status === "404") return;
     }
   }
+
+  return !0;
 }
