d666003663b1f17e1356c85ae8c1aec64f8bbd78
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 02:05:29 -0300

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -30,7 +30,7 @@ url = url || 'no encontrado'
 author = author || 'no encontrado'
     const vistas = formatViews(views)
     const canal = author.name ? author.name : 'Desconocido'
-    const infoMessage = `「⭐」Descargando *<${title || 'Desconocido'}>*\n\n> ⭐ Canal » *${canal}*\n> 👥 Vistas » *${vistas || 'Desconocido'}*\n> ⏳ Duración » *${timestamp || 'Desconocido'}*\n> 👤 Publicado » *${ago || 'Desconocido'}*\n> 🔗 Link » ${url}`
+    const infoMessage = `「⭐」Descargando *<${title || 'Desconocido'}>*\n\n> 👤 Canal » *${canal}*\n> 👥 Vistas » *${vistas || 'Desconocido'}*\n> ⏳ Duración » *${timestamp || 'Desconocido'}*\n> 🌐 Publicado » *${ago || 'Desconocido'}*\n> 🔗 Link » ${url}`
     const thumb = (await conn.getFile(thumbnail))?.data
     const JT = {
       contextInfo: {
