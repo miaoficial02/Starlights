6c95a928fc20de169f3f9ba0845cf1b7fcb6dc27
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 01:49:24 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,9 +5,9 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363416409380841@newsletter') return !0
+if (m.chat === '120363416409380800@newsletter') return !0
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, Soy Mia, creadora de Sumi he desactivado los comandos en los chats privados el cual serás bloqueado.`, false, {mentions: [m.sender]});
+    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, Soy Mia, creadora del bot he desactivado los comandos en los chats privados el cual serás bloqueado.`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
