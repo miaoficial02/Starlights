53cb369a6020d231e6e895693fb406a5c500f7c5
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 02:13:23 -0300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,12 +4,12 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 
 let grupos = `*Hola!, te invito a unirte a los grupos oficiales de ${botname}*
 
-- 𝐒𝐔𝐌𝐈 𝐒𝐀𝐊𝐔𝐑𝐀𝐒𝐀𝐖𝐀
-> *🌸* ${gp1}
+- 𝐒𝐓𝐀𝐑𝐋𝐈𝐆𝐇𝐓𝐒 💫
+> *⭐* ${gp1}
 
 *ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ*
 
-*✿ Enlace anulado? entre aquí! ✿*
+*★ Enlace anulado? entre aquí! ★*
       (𝐬𝐢𝐧 𝐠𝐫𝐮𝐩𝐨 𝐝𝐞 𝐫𝐞𝐬𝐩𝐚𝐥𝐝𝐨)
 
 > ${dev}`
