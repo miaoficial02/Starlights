a1c3ce38299aae9e3a15a54446fc46da443e7748
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 01:25:18 -0300

    Update owner-grouplist.js

--- plugins/owner-grouplist.js
+++ plugins/owner-grouplist.js
@@ -12,15 +12,15 @@ try {
     const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
     const participantStatus = isParticipant ? '👤 Participante' : '❌ Ex participante';
     const totalParticipants = participants.length;
-    txt += `*◉ Grupo ${i + 1}*
-    *➤ Nombre:* ${await conn.getName(jid)}
-    *➤ ID:* ${jid}
-    *➤ Admin:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
-    *➤ Estado:* ${participantStatus}
-    *➤ Total de Participantes:* ${totalParticipants}
-    *➤ Link:* ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || '--- (Error) ---'}` : '--- (No admin) ---'}\n\n`;
+    txt += `*✰ Grupo ${i + 1}*
+    *⭐ 𝐍𝐨𝐦𝐛𝐫𝐞:* ${await conn.getName(jid)}
+    *⭐ 𝐈𝐃:* ${jid}
+    *⭐ 𝐀𝐝𝐦𝐢𝐧:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
+    *⭐ 𝐄𝐬𝐭𝐚𝐝𝐨:* ${participantStatus}
+    *⭐ 𝐓𝐨𝐭𝐚𝐥 𝐝𝐞 𝐏𝐚𝐫𝐭𝐢𝐜𝐢𝐩𝐚𝐧𝐭𝐞𝐬:* ${totalParticipants}
+    *⭐ 𝐋𝐢𝐧𝐤:* ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || '--- (Error) ---'}` : '--- (No admin) ---'}\n\n`;
   }
-  m.reply(`*Lista de grupos del Bot* 🤖\n\n*—◉ Total de grupos:* ${totalGroups}\n\n${txt}`.trim());
+  m.reply(`*𝐋𝐢𝐬𝐭𝐚 𝐝𝐞 𝐠𝐫𝐮𝐩𝐨𝐬 𝐝𝐞𝐥 𝐁𝐨𝐭* ⭐\n\n*—◉ 𝐓𝐨𝐭𝐚𝐥 𝐝𝐞 𝐆𝐫𝐮𝐩𝐨𝐬:* ${totalGroups}\n\n${txt}`.trim());
 } catch {
   const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats);
   const totalGroups = groups.length;
@@ -33,15 +33,15 @@ try {
     const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
     const participantStatus = isParticipant ? '👤 Participante' : '❌ Ex participante';
     const totalParticipants = participants.length;    
-    txt += `*◉ Grupo ${i + 1}*
-    *➤ Nombre:* ${await conn.getName(jid)}
-    *➤ ID:* ${jid}
-    *➤ Admin:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
-    *➤ Estado:* ${participantStatus}
-    *➤ Total de Participantes:* ${totalParticipants}
-    *➤ Link:* ${isBotAdmin ? '--- (Error) ---' : '--- (No admin) ---'}\n\n`;
+    txt += `*✰ Grupo ${i + 1}*
+    *⭐ 𝐍𝐨𝐦𝐛𝐫𝐞:* ${await conn.getName(jid)}
+    *⭐ 𝐈𝐃:* ${jid}
+    *⭐ 𝐀𝐝𝐦𝐢𝐧:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
+    *⭐ 𝐄𝐬𝐭𝐚𝐝𝐨:* ${participantStatus}
+    *⭐ 𝐓𝐨𝐭𝐚𝐥 𝐝𝐞 𝐏𝐚𝐫𝐭𝐢𝐜𝐢𝐩𝐚𝐧𝐭𝐞𝐬:* ${totalParticipants}
+    *⭐ 𝐋𝐢𝐧𝐤:* ${isBotAdmin ? '--- (Error) ---' : '--- (No admin) ---'}\n\n`;
   }
-  m.reply(`*Lista de grupos del Bot* 👾\n\n*—◉ Total de grupos:* ${totalGroups}\n\n${txt}`.trim());
+  m.reply(`*𝐋𝐢𝐬𝐭𝐚 𝐝𝐞 𝐆𝐫𝐮𝐩𝐨𝐬 𝐝𝐞𝐥 𝐁𝐨𝐭* ⭐\n\n*—◉ 𝐓𝐨𝐭𝐚𝐥 𝐝𝐞 𝐆𝐫𝐮𝐩𝐨𝐬:* ${totalGroups}\n\n${txt}`.trim());
  }    
 };
 handler.help = ['groups', 'grouplist'];
