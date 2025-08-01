20aa4622aed95f58ac2a3952aae35621ad4f668d
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 21:12:30 -0300

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -3,13 +3,13 @@ import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
 let timestamp = speed()
-let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
+let sentMsg = await conn.reply(m.chat, '「⭐」 Calculando ping...', m)
 let latency = speed() - timestamp
 exec(`neofetch --stdout`, (error, stdout, stderr) => {
 let child = stdout.toString("utf-8");
 let ssd = child.replace(/Memory:/, "Ram:")
 
-let result = `✰ *¡Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n${ssd}`
+let result = `「⭐」 *¡Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n${ssd}`
 conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
 })
 }
