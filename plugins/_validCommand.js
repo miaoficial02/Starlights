e2fddcf843a8a94cfc209dfff0379c325e1a0f0c
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-31 22:54:00 -0300

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -29,5 +29,6 @@ user.commands = 0
 user.commands += 1
 } else {
 const comando = m.text.trim().split(' ')[0]
-await m.reply(`《⭐》Hola Amigo el comando *${comando}* no existe.\nPuedes ver mi lista de comandos disponibles usando:\n» *#help*`)
-}}
+  await conn.sendMessage(m.chat, { text: `╭━━⊱ ❌ 𝗘𝗦𝗧𝗘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢 𝗡𝗢 𝗘𝗫𝗜𝗦𝗧𝗘 ❌ ⊱━━╮\n┃ 💬 𝗛𝗼𝗹𝗮: @${m.sender.split("@")[0]}\n┃ 📌 𝗨𝘀𝗮: *#help*\n┃ 🧾 𝗣𝗮𝗿𝗮 𝘃𝗲𝗿 𝗹𝗼𝘀 𝗰𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗱𝗶𝘀𝗽𝗼𝗻𝗶𝗯𝗹𝗲𝘀\n╰━━━━━━━━━━━━━━━━━╯`, mentions: [m.sender] }, { quoted: m });
+//await m.reply(`《⭐》Hola Amigo el comando *${comando}* no existe.\nPuedes ver mi lista de comandos disponibles usando:\n» *#help*`)
+
